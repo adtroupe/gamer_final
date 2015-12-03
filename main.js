71e7312e774e46b2bd6d3623557af4848f834d5a
Chris Leatham <chris13@uw.edu>
2015-12-02 16:12:12 -0800

    Parlsey attempt

--- main.js
+++ main.js
@@ -148,4 +148,37 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 	$(document).ready(function() {
     	$('[data-toggle="popover"]').popover();   
 	});
+	$('form').parsley();
+	$(function () {
+  		var $sections = $('.form-section');
+
+  		function navigateTo(index) {
+    		$sections
+      			.removeClass('current')
+      			.eq(index)
+        		.addClass('current');
+    		$('.form-navigation .previous').toggle(index > 0);
+    		var atTheEnd = index >= $sections.length - 1;
+    		$('.form-navigation .next').toggle(!atTheEnd);
+    		$('.form-navigation [type=submit]').toggle(atTheEnd);
+  		}
+
+  		function curIndex() {
+    		return $sections.index($sections.filter('.current'));
+  		}
+
+  		$('.form-navigation .previous').click(function() {
+    		navigateTo(curIndex() - 1);
+ 		});
+
+  		$('.form-navigation .next').click(function() {
+    		if ($('.demo-form').parsley().validate('block-' + curIndex()))
+      			navigateTo(curIndex() + 1);
+  		});
+
+  		$sections.each(function(index, section) {
+    		$(section).find(':input').attr('data-parsley-group', 'block-' + index);
+  		});
+  		navigateTo(0); 
+	});
 })
