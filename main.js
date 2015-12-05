dbd58c48fb027ffbb1600d6b0ef6f0e30319f1bb
kailock <kailock@uw.edu>
2015-12-04 23:03:58 -0800

    working on multipage architecture

--- main.js
+++ main.js
@@ -131,13 +131,33 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider.state('profile', {
 		url: '/',
 		templateUrl: 'templates/profile.html',
-		controller: 'ProfileController',
+		controller: 'ProfileController'
 	})
-	.state('form', {
+	.state('new-game', {
 		url: '/new-game',
 		templateUrl: 'templates/new-game.html',
 		controller: 'NewGameController'
 	})
+	.state('game-entity', {
+		url: '/game-entity',
+		templateUrl: 'templates/game-entity.html',
+		controller: 'GameEntityController'
+	})
+		.state('franchise-entity', {
+			url: '/franchise-entity',
+			templateUrl: 'templates/franchise-entity.html',
+			controller: 'FranchiseEntityController'
+		})
+		.state('distribution-package-entity', {
+			url: '/distribution-package-entity',
+			templateUrl: 'templates/distribution-package-entity.html',
+			controller: 'DistributionPackageEntityController'
+		})
+		.state('local-release-entity', {
+			url: '/local-release-entity',
+			templateUrl: 'templates/local-release-entity.html',
+			controller: 'LocalReleaseEntityController'
+		})
 })
 
 .controller('ProfileController', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject) {
@@ -146,7 +166,7 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 
 .controller('NewGameController', function($scope) {
 	$(document).ready(function() {
-    	$('[data-toggle="popover"]').popover();   
+    	$('[data-toggle="popover"]').popover();
 	});
 	$('form').parsley();
 	$(function () {
@@ -179,6 +199,59 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
   		$sections.each(function(index, section) {
     		$(section).find(':input').attr('data-parsley-group', 'block-' + index);
   		});
-  		navigateTo(0); 
+  		navigateTo(0);
 	});
 })
+
+	.controller('GameEntityController', function($scope){
+
+	})
+	.controller('FranchiseEntityController', function($scope){
+
+	})
+	.controller('DistributionPackageEntityController', function($scope){
+
+	})
+	.controller('LocalReleaseEntityController', function($scope){
+
+	});
+
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+	if(animating) return false;
+	animating = true;
+
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+
+	//activate next step on progressbar using the index of next_fs
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+	//show the next fieldset
+	next_fs.show();
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'transform': 'scale('+scale+')'});
+			next_fs.css({'left': left, 'opacity': opacity});
+		},
+		duration: 800,
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		},
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
